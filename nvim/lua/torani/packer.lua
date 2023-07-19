d16b0e27db863e53de86ee71790261190966da5b
btmxh <ngoduyanh.chip@gmail.com>
2023-07-20 00:31:45 +0700

    feat: :sparkles: idk what's i'm doing btw

--- nvim/lua/torani/packer.lua
+++ nvim/lua/torani/packer.lua
@@ -61,16 +61,6 @@ return require('packer').startup(function(use)
   use 'nvim-tree/nvim-web-devicons'
 
   use {
-    'tonyfettes/fcitx5.nvim',
-    tag = 'v0.0.1-alpha',
-    rocks = { 'dbus_proxy', 'lgi' },
-    config = function()
-      require 'fcitx5'.setup {}
-      vim.cmd [[inoremap <S-space> <Cmd>lua require'fcitx5'.toggle()<CR>]]
-    end
-  }
-
-  use {
     'tummetott/reticle.nvim',
     config = function()
       require('reticle').setup {
