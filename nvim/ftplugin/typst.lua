268308987686812ae42634fb299867efa3736a28
btmxh <ngoduyanh.chip@gmail.com>
2024-03-31 14:17:37 +0700

    feat(nvim): :adhessive_bandage: typst preview

--- nvim/ftplugin/typst.lua
+++ nvim/ftplugin/typst.lua
@@ -1,3 +1,5 @@
 vim.opt.wrap = true
 vim.opt.textwidth = 80
-vim.api.nvim_buf_set_keymap(0, "n", "<leader>ll", ":TypstWatch<CR>", { noremap = true, desc = "typst: Watch file" });
+-- vim.api.nvim_buf_set_keymap(0, "n", "<leader>ll", ":TypstWatch<CR>", { noremap = true, desc = "typst: Watch file" });
+vim.api.nvim_buf_set_keymap(0, "n", "<leader>ll", "<cmd>!xdg-open %:p:r.pdf &<CR>", { noremap = true, desc = "typst: Open preview" });
+
