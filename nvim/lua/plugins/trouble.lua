0101740915930f8d9db543c8d7ef1ea839436fbb
btmxh <ngoduyanh.chip@gmail.com>
2024-06-30 13:04:14 +0700

    feat(nvim): :sparkles: upgrade Trouble
    also replaced with the default config because it works much better

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -1,8 +1,37 @@
 return {
-  'folke/trouble.nvim',
-  config = function()
-    vim.keymap.set("n", "<C-k>", function() require("trouble").next({ skip_groups = true, jump = true }) end)
-    vim.keymap.set("n", "<C-j>", function() require("trouble").previous({ skip_groups = true, jump = true }) end)
-    vim.keymap.set("n", "<leader>t", "<cmd>TroubleToggle<CR>", {desc="trouble: Toggle"})
-  end
+  "folke/trouble.nvim",
+  opts = {},
+  cmd = "Trouble",
+  keys = {
+    {
+      "<leader>xx",
+      "<cmd>Trouble diagnostics toggle<cr>",
+      desc = "Diagnostics (Trouble)",
+    },
+    {
+      "<leader>xX",
+      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+      desc = "Buffer Diagnostics (Trouble)",
+    },
+    {
+      "<leader>cs",
+      "<cmd>Trouble symbols toggle focus=false<cr>",
+      desc = "Symbols (Trouble)",
+    },
+    {
+      "<leader>cl",
+      "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+      desc = "LSP Definitions / references / ... (Trouble)",
+    },
+    {
+      "<leader>xL",
+      "<cmd>Trouble loclist toggle<cr>",
+      desc = "Location List (Trouble)",
+    },
+    {
+      "<leader>xQ",
+      "<cmd>Trouble qflist toggle<cr>",
+      desc = "Quickfix List (Trouble)",
+    },
+  },
 }
