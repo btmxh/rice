974c0b0c49e0ca9a50bcadf2c06aa518a54769d5
btmxh <ngoduyanh.chip@gmail.com>
2024-07-03 12:26:12 +0700

    fix(nvim): :bug: disable vimtex folding
    it's already done by the texlab lsp

--- nvim/lua/plugins/tex.lua
+++ nvim/lua/plugins/tex.lua
@@ -1,29 +1,26 @@
 return {
   'lervag/vimtex',
   ft = "tex",
-  config = function ()
+  config = function()
     vim.g.tex_flavor = 'latex'
     vim.g.vimtex_view_method = 'zathura'
     vim.g.vimtex_quickfix_mode = 0
-    vim.g.vimtex_fold_enable = 1
-    
+
     vim.g.tex_conceal = 'abdmg'
     vim.opt.conceallevel = 1
-    
-    vim.cmd([[
-    let g:vimtex_compiler_latexmk = {
-      \ 'aux_dir': 'out/',
-      \ 'out_dir': 'out/',
-      \ 'options': [
-      \   '-xelatex',
-      \   '-verbose',
-      \   '-file-line-error',
-      \   '-shell-escape',
-      \   '-synctex=1',
-      \   '-interaction=nonstopmode',
-      \   '-outdir=out'
-      \ ]
-    \ }
-    ]]);
+
+    vim.g.vimtex_compiler_latexmk = {
+      aux_dir = 'out/',
+      out_dir = 'out/',
+      options = {
+        '-xelatex',
+        '-verbose',
+        '-file-line-error',
+        '-shell-escape',
+        '-synctex=1',
+        '-interaction=nonstopmode',
+        '-outdir=out'
+      }
+    }
   end
 }
